cbaf919c061fd9df64483a9f2c411f1a6478cb0f
Tom <web@tomcanham.co.uk>
2025-02-19 20:00:47 +0000

    Finished initial css varaibles for colours and containers

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 export default function HomePage() {
   return (
-    <h1>This is going to be the home page of Pups Parlour</h1>
+    <div className="container">
+      <h1>This is going to be the home page of Pups Parlour</h1>
+    </div>
   )
 }
